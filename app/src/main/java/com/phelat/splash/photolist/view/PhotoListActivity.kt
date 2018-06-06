124e5bb0ffee25280e7ef641d7cffbe810961e86
Mohammad Mahdi Nouri <noorim39@yahoo.com>
2018-06-06 20:47:16 +0430

    Added photoListActivity (#9)

---
+++ app/src/main/java/com/phelat/splash/photolist/view/PhotoListActivity.kt
@@ -0,0 +1,19 @@
+package com.phelat.splash.photolist.view
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.phelat.splash.R
+
+/**
+ * Created by MAHDi on 6/6/18.
+ * Contact me m4hdi.pdroid at gmail.com
+ */
+
+class PhotoListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.photo_list_activity)
+    }
+
+}
