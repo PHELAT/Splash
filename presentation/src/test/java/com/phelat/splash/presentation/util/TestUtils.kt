2db633335ec1e696906ef561fabdc160bd497d85
mahdi <m4hdi.pdroid@gmail.com>
2018-07-27 21:13:37 +0430

    Added PhotoEntityToParcelPhotoEntityTest

---
+++ presentation/src/test/java/com/phelat/splash/presentation/util/TestUtils.kt
@@ -0,0 +1,29 @@
+package com.phelat.splash.data.util
+
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+
+object TestUtils {
+
+    fun readResource(fileName: String, classLoader: ClassLoader): String? {
+        val file = File(classLoader.getResource(fileName).file)
+        try {
+            BufferedReader(FileReader(file)).use { bufferedReader ->
+                val response = StringBuilder()
+                var line: String? = bufferedReader.readLine()
+                while (line != null) {
+                    response.append(line)
+                    response.append("\n")
+                    line = bufferedReader.readLine()
+                }
+                return response.toString()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return null
+        }
+
+    }
+
+}
