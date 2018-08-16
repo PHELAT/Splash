8505f92f507311441949ac4d4f39721d4c06899f
mahdi <m4hdi.pdroid@gmail.com>
2018-08-16 18:45:54 +0430

    Increased test coverage of PhotoUrlsToParcelPhotoUrls

--- presentation/src/test/java/com/phelat/splash/presentation/mapper/PhotoUrlsToParcelPhotoUrlsTest.kt
+++ presentation/src/test/java/com/phelat/splash/presentation/mapper/PhotoUrlsToParcelPhotoUrlsTest.kt
@@ -8,8 +8,9 @@ import com.phelat.splash.data.model.PhotoUrlsData
 import com.phelat.splash.data.response.PhotosResponse
 import com.phelat.splash.data.util.TestUtils
 import com.phelat.splash.presentation.model.ParcelPhotoUrlsData
-import org.hamcrest.MatcherAssert
+import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.core.IsEqual
+import org.hamcrest.core.IsNull
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -44,11 +45,50 @@ class PhotoUrlsToParcelPhotoUrlsTest {
 
         val parcelPhotoUrls = mapper.mapFromTo(photoEntity.photoUrls)
 
-        MatcherAssert.assertThat(parcelPhotoUrls!!.full, IsEqual(photoEntity.photoUrls!!.full))
-        MatcherAssert.assertThat(parcelPhotoUrls.raw, IsEqual(photoEntity.photoUrls!!.raw))
-        MatcherAssert.assertThat(parcelPhotoUrls.regular, IsEqual(photoEntity.photoUrls!!.regular))
-        MatcherAssert.assertThat(parcelPhotoUrls.small, IsEqual(photoEntity.photoUrls!!.small))
-        MatcherAssert.assertThat(parcelPhotoUrls.thumb, IsEqual(photoEntity.photoUrls!!.thumb))
+        assertThat(parcelPhotoUrls!!.full, IsEqual(photoEntity.photoUrls!!.full))
+        assertThat(parcelPhotoUrls.raw, IsEqual(photoEntity.photoUrls!!.raw))
+        assertThat(parcelPhotoUrls.regular, IsEqual(photoEntity.photoUrls!!.regular))
+        assertThat(parcelPhotoUrls.small, IsEqual(photoEntity.photoUrls!!.small))
+        assertThat(parcelPhotoUrls.thumb, IsEqual(photoEntity.photoUrls!!.thumb))
+
+    }
+
+    @Test
+    fun `test if ParcelPhotoUrls maps to PhotoUrlsData`() {
+
+        val photosResponse = Gson().fromJson(TestUtils.readResource("json/photos/photos_response.json",
+                this.javaClass.classLoader),
+                PhotosResponse::class.java)
+
+        val photoEntity = photoResponseToPhotoEntity.mapFromTo(photosResponse)
+
+        val parcelPhotoUrls = mapper.mapFromTo(photoEntity.photoUrls)
+
+        val photoUrls = mapper.mapToFrom(parcelPhotoUrls)
+
+        assertThat(photoUrls!!.full, IsEqual(photoEntity.photoUrls!!.full))
+        assertThat(photoUrls.raw, IsEqual(photoEntity.photoUrls!!.raw))
+        assertThat(photoUrls.regular, IsEqual(photoEntity.photoUrls!!.regular))
+        assertThat(photoUrls.small, IsEqual(photoEntity.photoUrls!!.small))
+        assertThat(photoUrls.thumb, IsEqual(photoEntity.photoUrls!!.thumb))
+
+    }
+
+    @Test
+    fun `test if mapFromTo returns null when input value is null`() {
+
+        val parcelPhotoUrls = mapper.mapFromTo(null)
+
+        assertThat(parcelPhotoUrls, IsNull())
+
+    }
+
+    @Test
+    fun `test if mapToFrom returns null when input value is null`() {
+
+        val parcelPhotoUrls = mapper.mapToFrom(null)
+
+        assertThat(parcelPhotoUrls, IsNull())
 
     }
 
